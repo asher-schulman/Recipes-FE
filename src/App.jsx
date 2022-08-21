2c95d6896453d0e1e7cf3dc0efed4cc4c69cecb0
Asher Schulman <asher.schulman@gmail.com>
2022-08-21 15:50:51 -0500

    test API call via the "TestCall.jsx component" complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import Counter from "./components/Counter";
+import TestCall from "./components/TestCall";
+import "./App.css";
+
+// /api/recipes
+// -> return all recipes with a future pagination implementation.
+
+// /api/recipes?ingredients=banana&ingredients=chocolate&title=bread
+// -> returns all recipes that have banana and chocolate as ingredients and bread in the title
+
+// /api/recipes/:id
+// -> pulls up all information on a specific recipe id
 
 function App() {
-  const [count, setCount] = useState(0)
+	return (
+		<div className="App">
+			<h1>Recipe App</h1>
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+			<TestCall />
+			<Counter />
+		</div>
+	);
 }
 
-export default App
+export default App;
---
+++ src/components/TestCall.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+const TestCall = () => {
+	// make api call to http://localhost:5000/api/recipes
+	// and log the response as json
+	// map the json to a list of <li> elements
+	// and render the list of <li> elements
+	const [data, setData] = useState([]);
+
+	const getData = async () => {
+		const response = await fetch("http://localhost:5000/api/recipes");
+		const json = await response.json();
+		setData(json);
+	};
+
+	useEffect(() => {
+		getData();
+	}, []);
+	return (
+		<div>
+			<h1>Test Call</h1>
+			<ul>
+				{data.map((item) => (
+					<li key={item._id.$oid}>{item.title}</li>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+export default TestCall;
